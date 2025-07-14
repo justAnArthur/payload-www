faa872ed7c78cbf2b3d78f31210ede88b35e60d8
Arthur Kozubov <kozubov.arthur@gmail.com>
2025-07-14 07:45:22 +0200

    chore: migrate to separate repositories with usage of git submodules

--- src/plugins/seo/src/exports/fields.ts
+++
@@ -1,5 +0,0 @@
-export { MetaDescriptionField } from '../fields/MetaDescription'
-export { MetaImageField } from '../fields/MetaImage'
-export { MetaTitleField } from '../fields/MetaTitle'
-export { OverviewField } from '../fields/Overview'
-export { PreviewField } from '../fields/Preview'
