cb89163326791089d97bd164aa7228f73545be2b
justAnArthur <kozubov.arthur@gmail.com>
2026-07-05 20:54:28 +0200

    refactor(globals,collections,www): rename globals to collections and reorganize exports

--- packages/payload-www/src/render/getFromImportMap.ts
+++ packages/payload-www/src/render/getFromImportMap.ts
@@ -1,5 +1,5 @@
 import type { ImportMap } from 'payload'
 
-export function getFromImportMap(key: string, importMap: ImportMap) {
-  return importMap[key.includes('#') ? key : key + '#default']
+export function getFromImportMap(key: string | undefined, importMap: ImportMap) {
+  return key && importMap[key.includes('#') ? key : key + '#default']
 }
